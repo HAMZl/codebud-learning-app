de7478f04a1bf85118b54e274d43fc02657480c3
Hamza Ahmed <sc21h2a@leeds.ac.uk>
2025-07-14 13:57:08 -0500

    Confirmed V2 embedding, rebuilt for deploy

--- frontend/flutter_app/build/web/flutter_service_worker.js
+++ frontend/flutter_app/build/web/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "de96c47ebfe816ad4d2e15601550d0a6
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "0a004321820784ad475f2832e4bd8fe6",
+"flutter_bootstrap.js": "7ccd23cfa06237fe214e8458b2b94bad",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
